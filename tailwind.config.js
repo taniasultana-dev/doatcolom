ff962c9ebb26cc5f76861ad72db41d89fd2ab545
taniasultana-dev <taniasultana.dev@gmail.com>
2023-11-02 19:42:12 +0600

    optimized section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,19 @@ module.exports = {
         blue:'#517DFC',
         gray:'#575757',
         black100:'#222121',
+        gray1:'#00000014',
+        blue1:'#0583D2',
+        yellow:'#FBAD13',
       },
       screens:{
-        'max-xs': '320px',
-        // => @media (min-width: 320px) { ... }
-  
+        xs: '320px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px'
+      },
+      boxShadow: {
+        '3xl': '8px 24px 40px 8px #00000014',
       },
     fontFamily: {
       lato: ['Lato','sans-serif'],
